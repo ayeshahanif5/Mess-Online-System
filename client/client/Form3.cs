061d6cb963551f32408277218cb45c14af37a54b
aslamayesha <aslamayesha799@gmail.com>
2018-04-19 10:20:51 +0500

    i have added the wire frame and functions of forget password

---
+++ client/client/Form3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool osfound, found;
+            localhost.Service1 s = new localhost.Service1();
+            s.forget_password(comboBox1.Text, textBox1.Text, textBox2.Text, out osfound, out found);
+            if (osfound)
+            {
+                MessageBox.Show("changed");
+            }
+            else { MessageBox.Show("not changed"); }
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
